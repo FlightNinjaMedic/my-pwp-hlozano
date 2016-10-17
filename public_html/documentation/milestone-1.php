a730578d3c093005f285b09ed2971db8c5bbcf95
Hector Lozano <hlozano2@cnm.edu>
2016-10-17 17:08:48 -0600

    added scrolling title

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,10 +5,12 @@
 
 		<title> Hector's Page</title>
 	</head>
-	<header><h1> Welcome To The Medical Cannabis Advocate Page</h1></header>
+	<header><h1> <marquee behavior="scroll" direction="left">The Medical Cannabis Advocate Site</marquee>
+		</h1></header>
 	<body>
 		<section>
-			<p></p>
+			<h1>Mission Statement</h1>
+			<p> The Medical Cannabis Advocate site comes to life with the sole purpose to raise awareness on the benefits of medical cannabis, specifically the cannabinoid CBD: cannabidiol and how important it is in treatin</p>
 		</section>
 	</body>
 </html>
