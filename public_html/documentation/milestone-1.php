21297e69f8fe20dc3478cf0a44b7eef0d217186e
Hector Lozano <hlozano2@cnm.edu>
2016-10-17 14:39:21 -0600

    added milestone.php file

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<title> Hector's Page</title>
+	</head>
+	<header><h1> Welcome To The Medical Cannabis Advocate Page</h1></header>
+	<body>
+		<section>
+			<p></p>
+		</section>
+	</body>
+</html>
