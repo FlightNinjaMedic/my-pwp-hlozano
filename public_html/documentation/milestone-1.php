c5e912f86a3f11f37df81f76289283f43dcf1b87
Hector Lozano <hlozano2@cnm.edu>
2016-10-18 11:36:44 -0600

    removed scrolling title and began creating sections

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,12 +5,26 @@
 
 		<title> Hector's Page</title>
 	</head>
-	<header><h1> <marquee behavior="scroll" direction="left">The Medical Cannabis Advocate Site</marquee>
-		</h1></header>
+	<header><h1>The Medical Cannabis Advocate Site</h1></header>
+
 	<body>
 		<section>
-			<h1>Mission Statement</h1>
-			<p> The Medical Cannabis Advocate site comes to life with the sole purpose to raise awareness on the benefits of medical cannabis, specifically the cannabinoid CBD: cannabidiol and how important it is in treatin</p>
+			<h1>Purpose</h1>
+			<p> The Medical Cannabis Advocate site comes to life with the purpose to educate and raise awareness on the benefits of medical cannabis, specifically the cannabinoid CBD: cannabidiol and how important and succesful it is in treating a condition called: Dravet Syndrome. </p>
+		</section>
+
+		<section>
+			<h1> Audience </h1>
+			<p> The site will be beneficial for everyone but it's main focus will be to reach families/parents of children suffering from Dravet Syndrome or any other type of seizure disorders and provide them with a site they can visit and get educational material on the use of Rick Simpson Oil to treat this devilitating condition. It will also provide information on the current states that currently participate/ have a medical cannabis program and the specific laws governing such program.</p>
+		</section>
+
+		<section>
+			<h1>Goal</h1>
+			<ul>
+				<li>Provide Information To Parents Of Children With Devilitating Seizure Disorders Such As Dravet Syndrome.</li>
+				<li>Provide a site where parents can post questions or link up with other parents and discuss concerns and get answers to their questions. </li>
+				<li></li>
+			</ul>
 		</section>
 	</body>
 </html>
