c039b1e7da25abb35f95e2678eb26c5809237ef7
Hector Lozano <hlozano2@cnm.edu>
2016-10-30 20:25:32 -0600

    Finished write up, started on browser wireframe

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,9 +9,11 @@
 		This will be a simple and down to earth one liner that will make the user feel warm and fuzzy when visiting the site.
 		</p>
 		<h1> Information Page</h1>
-		This section will feature information specific to the NMMCP (New Mexico Medical Cannabis Program as well as the latest on different neurological conditions being trated with medical cannabis. It will also feature stories from around the nation on infants being treated with the hemp concentrates.
+		<p>This section will feature information specific to the NMMCP (New Mexico Medical Cannabis Program as well as the latest on different neurological conditions being trated with medical cannabis. It will also feature stories from around the nation on infants being treated with the hemp concentrates. This section will also contain links to other medical cannabis oriented sites
+		</p>
+		<h1>Questions or Concerns</h1>
 		<p>
-
+			In this section the user will have the option to join our mailing list and even have the option to be contacted by us by either e-mail or phone call.
 		</p>
 
 	</body>
