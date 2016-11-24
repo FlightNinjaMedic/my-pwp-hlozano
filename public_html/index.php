9dbf3588b70397b63a2e96915a30a4cb13b8c44f
Hector Lozano <hlozano2@cnm.edu>
2016-11-23 20:43:09 -0700

    added scrolling marquee

--- public_html/index.php
+++ public_html/index.php
@@ -65,7 +65,7 @@
 					<img src="images/rambohm1.jpg" alt="rambohm1">
 				</div>
 				<div class="col-md-3">
-					Local Albuquerque resident, medical cannabis card holder and developer of The Medical Cannabis Advocate Site, Hector Lozano, is now in his third year using medical cannabis products to treat PTSD. Mr Lozano is a 51 year old former navy corpsman with two deployments in support of Operation Iraqi Freedom and Operation Endurng Freedom. Mr Lozano also enjoyed a rewarding and sucessful career as a field paramedic, emergency room paramedic and crirical care flight paramedic
+					Local Albuquerque resident, medical cannabis card holder and developer of The Medical Cannabis Advocate Site, Hector Lozano, is now in his third year using medical cannabis products to treat PTSD. Mr Lozano is a 51 year old former navy corpsman with two deployments in support of Operation Iraqi Freedom and Operation Enduring Freedom. Mr Lozano also enjoyed a rewarding and sucessful career as a field paramedic, emergency room paramedic and crirical care flight paramedic
 				</div>
 			</div><br><br>
 
